d7271751dffb6fac729cd548b33c25b818a07f6d
Richard Wendel <rwendel2@illinois.edu>
2016-09-18 04:06:47 +0000

    working on face rec

--- drone/testpicture.py
+++ drone/testpicture.py
@@ -1,29 +1,73 @@
-import cv2
+import cv2, os
+import numpy as np
 import urllib
+import PIL import Image
 
 
-urllib.urlretrieve("https://raw.githubusercontent.com/Itseez/opencv/master/data/haarcascades/haarcascade_frontalface_alt.xml","haarcascade_frontalface_alt.xml")
 
-cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
+def get_images_and_labels(path,faceCascade):
+    image_paths = [];
+    x = 0
+    while(x<9):
+        image_paths.append("%sm_pic_%d.png" % (path,x));
+        image_paths.append("%sr_pic_%d.png" % (path,x));
+    images = []
+    labels = []
 
-cv2.imwrite('rich_pic.png',frame)
+    for image_path in image_paths:
+        img = cv2.imread(image_path);
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        image_pil = np.array(gray, 'uint8')
+        nbr = 0;
+        if "m" in image_path:
+            nbr = 1;
 
-img = frame;
+        faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5, minSize=(30, 30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
 
-# And convert to gray-scale for processing
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        for(x,y,w,h) in faces:
+            images.append(image[y: y+h, x: x+w])
+            labels.append(nbr)
+            print "appending pic with nbr%d" % (nbr);
 
-# Create the classifier and run the algorithm
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
-faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5, minSize=(30, 30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+    return images, labels
 
-# For each face found in the image, draw a box around it
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+def predict(recognizer, img_path_to_test, nbr):
+    img = cv2.imread(img_path_to_test);
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    image_pil = np.array(gray, 'uint8')
+    faces = faceCascade.detectMultiScale(image_pil)
+    nbr_predicted, conf = recognizer
+    
 
-# Write the image back out to a file
-cv2.imwrite("rich_facefound.png",img)
 
-cap.release()
-exit()
+
+
+
+def take_pic(save_file_path):
+    ret, frame = cap.read()
+    cv2.imwrite(save_file_path,frame)
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    ##get cascase xml file
+    urllib.urlretrieve("https://raw.githubusercontent.com/Itseez/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml","haarcascade_frontalface_default.xml")
+
+    # Create the classifier and run the algorithm
+    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    recognizer = cv2.createLBPHFaceRecognizer()
+
+    path = "img/"
+    images, labels = get_images_and_labels(path,faceCascade)
+    recognizer.train(images, np.array(labels))
+
+
+
+
+    #cv2.imwrite("rich_facefound.png",img)
+
+    cap.release()
+    exit()
+
+
+if __name__ == "__main__": main()
